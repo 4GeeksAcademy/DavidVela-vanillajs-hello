e0e62fc279af9bc1fa5241b00187278379efa02f
DavidVela1712 <75803006+DavidVela1712@users.noreply.github.com>
2025-05-13 19:23:54 +0000

    130525-Fin-Proyecto

--- src/app.js
+++ src/app.js
@@ -10,7 +10,17 @@ let action = ['ate', 'peed', 'crushed', 'broke'];
 let what = ['my homework', 'my phone', 'the car'];
 let when = ['before the class', 'when I was sleeping', 'while I was exercising', 'during my lunch', 'while I was praying'];
 
+let randomN = 0;
+
+let frase = "";
+
 window.onload = function() {
   //write your code here
+  frase = who[randomNumber()] +" "+ action[randomNumber()] +" "+ what[randomNumber()] +" "+ when[randomNumber()];
+  document.getElementById("excuse").innerHTML = frase;
   console.log("Hello Rigo from the console!");
 };
+
+function randomNumber() {
+  return Math.floor(Math.random()*3);
+}
